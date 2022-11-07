866a24a01a2edacd24bd0bfdacfe146d28fb0858
Mikael Grön <skaramicke@gmail.com>
2022-11-07 08:57:43 +0100

    Change how hunger and reproduction counting is initialized to remove the synchronized population changes

--- src/creature.js
+++ src/creature.js
@@ -13,8 +13,8 @@ class Creature {
 
     this.reproduce = false;
 
-    this.reproduceCounter = Math.random() * 0.1;
-    this.hunger = parent ? parent.hunger : Math.random();
+    this.reproduceCounter = parent ? 0 : Math.random();
+    this.hunger = parent ? 0 : Math.random();
     this.starved = false;
 
     this.r = 5.0;
